24a452cfee57f927b00e2501a76f214f0bc7af66
Moises Valdez <moisesvaldez075@gmail.com>
2020-07-13 22:42:35 -0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 import './App.css';
 
 class App extends Component {
@@ -26,14 +27,14 @@ class App extends Component {
 
         return (
             <div className='App'>
-                <input
-                    onChange={e =>
+                <h1>Monster Rolodex</h1>
+                <SearchBox
+                    placeholder='Search Monster'
+                    handleChange={e =>
                         this.setState({ searchField: e.target.value })
                     }
-                    type='search'
-                    placeholder='Search Monster'
                 />
-                <CardList monsters={filterMonsters} />
+                <CardList monsters={filterMonsters} />{' '}
             </div>
         );
     }
